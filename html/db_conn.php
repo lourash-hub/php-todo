3e7e28c50c1ac22bfcccfa139f37ca62815214b2
Lourash <lourash@LouRash>
2024-07-04 10:57:13 -0500

    first commit

---
+++ html/db_conn.php
@@ -0,0 +1,26 @@
+<?php
+
+//connect .env file
+
+require_once realpath(__DIR__ . "/vendor/autoload.php");
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// enabling environment variable for php
+
+$servername = "mysqlserverhost";
+$username = "lourash";
+$password = "Password@123";
+$dbname = "toolingdb";
+
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
